39282a0f682592ef6c36f743ae2ae3353f077b7e
None <rikki-tamaki@osc-corp.co.jp>
2015-12-11 10:29:11 +0000

    既存のbookmarkにvpos_lengthを追加

--- db/migrate/20151209125501_add_video_length_to_bookmark.rb
+++ db/migrate/20151209125501_add_video_length_to_bookmark.rb
@@ -1,5 +1,26 @@
 class AddVideoLengthToBookmark < ActiveRecord::Migration
+  require 'net/https'
+  require 'uri'
+  require 'json'
+
   def change
     add_column :bookmarks, :video_length, :integer, null: false
+    Bookmark.reset_column_information
+    Bookmark.all.each do |bookmark|
+      bookmark.video_length = get_nicovideo_thumb_response(bookmark.smid)[:thumb][:length]
+      bookmark.save
+    end
   end
+
+  def get_nicovideo_thumb_response(id_info)
+    request = "http://ext.nicovideo.jp/api/getthumbinfo/#{id_info}"
+
+    uri = URI.parse(request)
+    xml = Net::HTTP.get(uri)
+    json = Hash.from_xml(xml).to_json
+    nico_thumb_info = JSON.parse(json, {:symbolize_names => true })
+
+    return nico_thumb_info[:nicovideo_thumb_response]
+  end
+
 end
